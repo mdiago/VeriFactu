60c1a1d01195867066d9ea2b6d2e2f7620fe03ae
Manuel Diago García <manuel@manuelyelena.com>
2025-03-27 19:22:01 +0100

    Añade info comentarios causas exención
    Añade info comentarios causas exención

--- NetCore/Src/Xml/Factu/Alta/CausaExencion.cs
+++ NetCore/Src/Xml/Factu/Alta/CausaExencion.cs
@@ -53,27 +53,29 @@ namespace VeriFactu.Xml.Factu.Alta
         NA,
 
         /// <summary>
-        /// Exenta por el artículo 20.
+        /// Exenta por el artículo 20 (Exenciones en operaciones interiores).
         /// </summary>
         E1,
 
         /// <summary>
-        /// Exenta por el artículo 21.
+        /// Exenta por el artículo 21 (Exportaciones).
         /// </summary>
         E2,
 
         /// <summary>
-        /// Exenta por el artículo 22.
+        /// Exenta por el artículo 22 (Operaciones asimiladas a las
+        /// exportaciones: Navegación marítima internacional, aeronaves...).
         /// </summary>
         E3,
 
         /// <summary>
-        /// Exenta por los artículos 23 y 24.
+        /// Exenta por los artículos 23 y 24 (Exenciones relativas a
+        /// regímenes aduaneros y fiscales: Depositos aduaneros...).
         /// </summary>
         E4,
 
         /// <summary>
-        /// Exenta por el artículo 25.
+        /// Exenta por el artículo 25 (Operaciones UE).
         /// </summary>
         E5,
 
